3dcc2d30b3b0a516cc40a5847f182888aeb195ed
Danny Petrilli <dannypetrilli@gmail.com>
2023-05-18 16:52:36 -0400

    refactor vimeo component pt 2

--- storyblok/Vimeo.vue
+++ storyblok/Vimeo.vue
@@ -3,6 +3,7 @@
     ref="vimeoPlayer"
     class="vimeo-player"
     v-editable="blok"
+    :style="{ paddingTop: padding }"
   >
     <iframe
       ref="vimeoIframe"
@@ -17,7 +18,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed, onUnmounted, watch } from "vue";
 import Player from "@vimeo/player";
 
 defineProps({
@@ -31,45 +32,67 @@ defineProps({
 const vimeoPlayer = ref(null);
 const vimeoIframe = ref(null);
 const allow = "autoplay;";
+let aspectRatio = ref(0);
+let portraitMaxHeight = 600; // Maximum acceptable height for portrait videos. Adjust as needed.
+let windowWidth = ref(window.innerWidth);
+
+const updateWindowWidth = () => {
+  windowWidth.value = window.innerWidth;
+};
 
 onMounted(async () => {
+  window.addEventListener("resize", updateWindowWidth);
+
   const player = new Player(vimeoIframe.value);
 
   player.ready().then(async () => {
     const videoWidth = await player.getVideoWidth();
     const videoHeight = await player.getVideoHeight();
 
-    if (videoWidth === videoHeight) {
-      vimeoPlayer.value.classList.add("square");
-    } else if (videoWidth > videoHeight) {
-      vimeoPlayer.value.classList.add("landscape");
-    } else {
-      vimeoPlayer.value.classList.add("portrait");
-    }
+    aspectRatio.value = videoHeight / videoWidth;
   });
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateWindowWidth);
+});
+
+const padding = computed(() => {
+  if (windowWidth.value <= 768) {
+    // Mobile breakpoint
+    if (aspectRatio.value > 1) {
+      // Reduce padding for portrait videos on larger screens
+      return Math.min(aspectRatio.value * 100, window.innerHeight * 0.8) + "%"; // 80% of window height
+    } else {
+      // Default padding for square and landscape videos
+      return aspectRatio.value * 100 + "%";
+    }
+  } else {
+    // Default padding for landscape videos on desktop
+    return "56.25%";
+  }
 });
 </script>
 
 <style scoped>
 .vimeo-player {
   width: 100%;
+  position: relative;
   overflow: hidden;
 }
 
 .vimeo-player iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
   border: none;
 }
 
-.vimeo-player.square iframe {
-  aspect-ratio: 1 / 1; /* 1:1 aspect ratio for square videos */
-}
-
-.vimeo-player.landscape iframe {
-  aspect-ratio: 16 / 9; /* 16:9 aspect ratio for landscape videos */
-}
-
-.vimeo-player.portrait iframe {
-  aspect-ratio: 3 / 3.5; /* 3:3.5 aspect ratio for portrait videos */
+@media (min-width: 769px) {
+  .vimeo-player {
+    height: auto;
+  }
 }
 </style>
